5d44c036287ba47a044eae1b7d011dc84d07ef58
Jerry Zheng <jerry.zheng@hotmail.com>
2020-09-19 12:15:18 -0400

    Basic code for having item in hand and throwing

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour  
+{
+    public GameObject item = null;
+
+    public void Pickup(GameObject item){
+        this.item = item;
+    }
+}
---
+++ Assets/Scripts/ThrowItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowItem : MonoBehaviour
+{
+    public int XVector;
+    public int YVector;
+
+    public void Throw(){
+        GameObject item = gameObject.GetComponent<Inventory>().item;
+        if (item){
+            Rigidbody rb = item.GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(20, 20, 0);
+        }
+    }
+
+    void Update ()
+    {
+        bool down = Input.GetKeyDown(KeyCode.Space);
+        if(down)
+        {
+            Throw();
+        }
+    }
+}
